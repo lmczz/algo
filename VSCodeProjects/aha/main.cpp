8ac266ddf54a84c7de560f19d2682ebc14644284
lmc <lmczz@qq.com>
2019-11-11 00:23:51 +0800

    test

--- VSCodeProjects/aha/main.cpp
+++ VSCodeProjects/aha/main.cpp
@@ -3,17 +3,62 @@
 #include <unistd.h>
 using namespace std;
 void doudou() {
-    int pw,ld=0;
+    int ld=0,num=0;
+    char pswd,press,gm;
+    string name;
     cout<<"londing..."<<endl;
     while(1){
         cout<<"%"<<ld<<" ";
         fflush(stdout);
         sleep(1);
-        // usleep(10000);
-        // ld++;
-        // if(ld==100){
-        //    break;
-        // }
+        ld++;
+        if(ld>10){
+            break;
+        }
+    }
+    cout<<endl;
+    while(1){
+        cout<<"what's the password?"<<endl;
+        cin>>pswd;
+        if(pswd=='l'){
+            break;
+        }
+    }
+    cout<<"Hello,what'your name?"<<endl;
+    cin>>name;
+    cout<<"Hello ^_^,what can I do for you?"<<endl;
+    sleep(1);
+    cout<<"notice: press 'e' to end this program"<<endl;
+    sleep(1);
+    cout<<"notice: press 'p' to play a game"<<endl;
+    while(1){
+        cin>>press;
+        if(press=='e'){
+            cout<<"This is program 1.0"<<endl;
+            cout<<"made with wenboLi"<<endl;
+            cout<<"Have a good time!Bye! ^_^"<<endl;
+            return;
+        }else if(press=='p'){
+            cout<<"please wait..."<<endl;
+            sleep(3);
+            cout<<"press 'l' to leave"<<endl;
+            cout<<"press 'n' to type a number(1,2,3...)"<<endl;
+            cout<<"press other letters there will a suprise!"<<endl;
+            cout<<"let's play!"<<endl;
+            while(1){
+                cin>>gm;
+                if(gm=='n'){
+                    cout<<num;
+                    fflush(stdout);
+                    num++;
+                }else if(gm=='l'){
+                    break;
+                }else{
+                    cout<<"`~!@#$%^&*()_+"<<endl;
+                }
+            }
+        }
+        
     }
 }
 int main(){
--- VSCodeProjects/aha/sort.c
+++ VSCodeProjects/aha/sort.c
@@ -1,5 +1,144 @@
+#include <stdio.h>
+
+void print_array(int a[], int len)
+{
+    for (size_t i = 0; i < len; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+void init_array(int a[], int len)
+{
+    for (size_t i = 0; i < len; i++)
+    {
+        a[i] = 0;
+    }
+}
+
+int sort_tong(int a[], int len)
+{
+    int MAX = 10000;
+    int book[MAX];
+    init_array(book, MAX);
+    for (int i = 0; i < len; i++)
+    {
+        book[a[i]] += 1;
+    }
+
+    int j = 0;
+    for (int i = 0; i < MAX; i++)
+    {
+        // printf("i is:%d, book value is %d\n", i,book[i]);
+        if (book[i] > 0)
+        {
+            for (int k = 0; k < book[i]; k++)
+            {
+                // printf("j is:%d\n", j);
+                a[j++] = i;
+            }
+        }
+    }
+    return 0;
+}
+
+int sort_maopao(int a[], int len)
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        // printf("i====%d\n",i );
+        for (int j = 0; j < len - 1 - i; j++)
+        {
+            // printf("j=%d\n",j);
+            if (a[j] > a[j + 1])
+            {
+                int tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+            }
+        }
+    }
+    return 0;
+}
+
+void swap(int a[], int i, int j)
+{
+    if (i == j)
+    {
+        return;
+    }
+    printf("swap %d[%d], %d[%d]\n", a[i],i,a[j],j);
+    int tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+}
+
+int sort_quick(int a[], int start, int end)
+{
+    printf("sort_quick,start:%d, end:%d\n", start, end);
+    if (start >= end)
+    {
+        return 0;
+    }
+
+    int base = a[start];
+    int i = start;
+    int j = end;
+    while (1)
+    {
+        // move j
+        while (j > i && a[j] >= base)
+        {
+            j--;
+        }
+
+        // if (j == i)
+        // { // end
+        //     if (a[start] > a[i])
+        //         swap(a, start, i); //i should be small value
+        //     break;
+        // }
+        // else
+        // { // j find small vale
+        //     //move i
+            while (i < j && a[i+1] <= base)
+            {
+                i++;
+            }
+            if (i == j)
+            {
+                if (a[start] > a[i])
+                    swap(a, start, i); //i should be small value
+                break;
+            }
+            else
+            { // swap i(small), j(larger)
+                swap(a, i, j);
+                print_array(a,8);
+            }
+        // }
+    }
+    print_array(a,8);
+
+    // left and right
+    printf("--i is %d\n", i);
+    sort_quick(a, start, i - 1);
+    printf("==i is %d\n", i);
+    sort_quick(a, i + 1, end);
+
+    return 0;
+}
 int main(int argc, char const *argv[])
 {
     /* code */
+    int alen = 8;
+
+    int a[] = {999, 5, 5, 4, 4, 3, 2, 1};
+    print_array(a, alen);
+    // sort_tong(a, alen);
+    // sort_maopao(a, alen);
+    sort_quick(a, 0, alen - 1);
+    print_array(a, alen);
     return 0;
 }
